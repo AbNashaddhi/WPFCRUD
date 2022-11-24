2e9040bddaf80a349e1ec52f453e2cddb07cb19c
AbNashaddhi <117175615+AbNashaddhi@users.noreply.github.com>
2022-11-24 10:33:57 +0530

    Add files via upload

---
+++ WPFCRUDOP/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFCRUDOP
+{
+   public  class Employee
+    {
+        public virtual int Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Age { get; set; }
+        public virtual string Salary { get; set; }
+        public virtual string Designation { get; set; }
+    }
+}
---
+++ WPFCRUDOP/SessionClass.cs
@@ -0,0 +1,15 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+namespace WPFCRUDOP
+{
+    public class SessionClass
+    {
+        public static ISession OpenSession()
+        {
+            var configuration = new Configuration();
+            configuration.Configure();
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
